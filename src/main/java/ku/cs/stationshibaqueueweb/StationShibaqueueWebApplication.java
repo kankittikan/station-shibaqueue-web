6d7e2500a52bc38dc8acbadec8f6652a6f48b155
kankittikan <nongkan195@gmail.com>
2023-10-03 19:59:37 +0700

    init spring

---
+++ src/main/java/ku/cs/stationshibaqueueweb/StationShibaqueueWebApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.stationshibaqueueweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationShibaqueueWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StationShibaqueueWebApplication.class, args);
+    }
+
+}
